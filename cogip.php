9f8200c35de40209918cae89746af69233977288
MorganeMeganck <morgane.meganck@gmail.com>
2018-08-30 12:14:59 +0200

    crud personnes

---
+++ cogip.php
@@ -0,0 +1,82 @@
+<?php
+require 'sql.php';
+$resultat = $bdd->query('SELECT * FROM Personnes');
+ob_start();
+while ($donnees = $resultat->fetch())
+{
+  echo '<td>'.$donnees['Nom'].'</td>';
+  echo '<td>'.$donnees['Prenom'].'</td>';
+  echo '<td>'.$donnees['Telephone'].'</td>';
+  echo '<td>'.$donnees['Email'].'</td>';
+  echo '<td>'.'<a href="delete.php?id='. $donnees['id'].'">'.'Supprimer</a></td>';
+  echo '<td>'.'<a href="update.php?id='. $donnees['id'].'">'.'Modifier</a></td></tr>';
+}
+$resultat->closeCursor();
+;
+
+$out1 = ob_get_contents();
+ob_end_clean();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>COGIP</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+  <script src="main.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <ul id="dropdown1" class="dropdown-content">
+      <li><a href="#!"> Historique des factures</a></li>
+      <li><a href="#!">Créer une facture</a></li>
+    </ul>
+    <nav>
+       <div class="nav-wrapper red darken-4">
+         <a href="index.html" class="brand-logo "><img class="responsive-img" id="logo" src=""/></a>COGIP</a>
+         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+         <ul id="nav-mobile" class="right hide-on-med-and-down">
+           <li><a href="cogip.php">Accueil</a></li>
+           <!-- Dropdown Trigger -->
+           <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Factures<i class="material-icons right">arrow_drop_down</i></a></li>
+           <li><a href="#">Sociétés</a></li>
+           <li><a href="cogip.php">Personnes</a></li>
+         </ul>
+       </div>
+    </nav>
+    <ul class="sidenav" id="mobile-demo">
+      <li><a href="cogip.php">Accueil</a></li>
+      <li><a href="#">Factures</a></li>
+      <li><a href="#">Sociétés</a></li>
+      <li><a href="cogip.php">Historique de client</a></li>
+    </ul>
+    <!-- Fin navbar -->
+    <div class="container">
+    <h3>Historique de clients</h3>
+    <a class="waves-effect waves-light btn red darken-4" onclick="window.print();return false;">Imprimer</a>
+    <a href="create.php" class="waves-effect waves-light btn red darken-4">Créer un nouveau client</a>
+      <table>
+        <thead>
+          <tr>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Telephone</th>
+              <th>Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php echo $out1; ?>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
---
+++ create.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ajouter un client</title>
+	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+	<a href="/cogip.php">Liste des données</a>
+	<h1>Ajouter</h1>
+	<form action="createphp.php" method="post">
+		<div>
+			<label for="Nom">Nom</label>
+			<input type="text" name="Nom" value="">
+		</div>
+
+		<div>
+			<label for="Prenom">Prénom</label>
+			<input type="text" name="Prenom" value="">
+		</div>
+
+		<div>
+			<label for="Telephone">Telephone</label>
+			<input type="number" name="Telephone" value="">
+		</div>
+		<div>
+			<label for="Email">Email</label>
+			<input type="text" name="Email" value="">
+		</div>
+		<button type="submit" name="button">Envoyer</button>
+	</form>
+</body>
+</html>
