6ad72fce912e5af9d104d1145e950c74c459bfb5
baptistefkt <baptistefirket@gmail.com>
2018-09-05 12:03:43 +0200

    update factureVue + factureDetail

---
+++ Vue/detailFactureVue.php
@@ -0,0 +1,24 @@
+<?php
+include 'header.php';
+require '../Model/sql.php';
+require '../Model/detailFactureModel.php';
+ ?>
+
+    <div class="container">
+        <h3>Détail de la facture n° : <?php echo $factureNum;?></h3>
+        <a class="waves-effect waves-light btn cyan darken-2" onclick="window.print();return false;">Imprimer</a>
+        <h5>Date : </h5>
+        <h6><?php echo $factureDate; ?></h6>
+        <h5>Société liée à la facture :</h5>
+        <h6> <?php echo $factureSoc; ?></h6>
+        <h5>Type de la société :</h5>
+        <h6> <?php echo $factureType; ?></h6>
+        <h5>Contact lié à la facture :</h5>
+        <h6> <?php echo $facturePrenom.' '.$factureNom; ?></h6>
+        <h5>Objet de la facture :</h5>
+        <p><?php echo $factureObj; ?></p>
+    </div>
+
+<?php
+include 'footer.php';
+ ?>
