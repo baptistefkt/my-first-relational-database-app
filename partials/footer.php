2a273fa91094522d0cd75a80d503309d73ad4743
MorganeMeganck <morgane.meganck@gmail.com>
2018-09-05 10:56:42 +0200

    herokustart

---
+++ partials/footer.php
@@ -0,0 +1,39 @@
+<!--  Footer-->
+<div class="row">
+  <div class="footer_copyright">
+    <div id="cookieConsent">
+        <div id="closeCookieConsent">x</div>
+        This website is using cookies. <a href="#" target="_blank" name="cookie">More info</a>. <a class="cookieConsentOK">That's Fine</a>
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInDown"  data-wow-duration="3s">
+      <div class="single_copyirgth text-left">
+        <p>Copyright 2016 &copy;  by Morgane Meganck, Ajay. Based from the pink donut template from XS@mail.com</p>
+      </div>
+    </div>
+    <div class="col-md-6 col-sm-6 col-xs-12">
+      <div class="single_copyirgth wow fadeInUp"  data-wow-duration="2s">
+        <ul class="navbar-right">
+          <li class="active"><a href="home.php" name="home">Accueil</a></li>
+          <li><a href="agenda.php" name="agenda">Agenda</a></li>
+          <li><a href="formulaire.php" name="contact">Contact</a></li>
+        </ul>
+      </div>
+     </div>
+    </div>
+</div>
+</div>
+<!-- STRAT SCROLL TO TOP -->
+<div class="scrollup">
+    <a aria-label="chevronfb" href="facebook.com" id="chevronfb"><i class="fa fa-chevron-up"></i></a>
+</div>
+<script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<script src="assets/js/vendor/isotope.min.js"></script>
+<script src="assets/js/jquery.mixitup.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/main.js"></script>
+<script type="text/javascript">
+    document.getElementById("myButtonagenda").onclick = function () {
+        location.href = "agenda.html";
+    };
+</script>
